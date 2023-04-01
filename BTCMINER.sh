8509fa2c038536a8bb43b71733ad33189984a510
Codeman04TheFreaking2nd <127219550+Codeman04TheFreaking2nd@users.noreply.github.com>
2023-04-01 11:36:50 -0400

    holy shit! 295 lines of code added in one update
    :supershock: ; :trollface:

--- BTCMINER.sh
+++ BTCMINER.sh
@@ -1,25 +1,294 @@
 #!/bin/bash
 
 # Change the following variables to match your configuration
-address="YOUR BTC ADDRESS GOES HERE!"
-pool_url="stratum+tcp://us-east.stratum.slushpool.com:3333"
-worker_name="worker1"
-worker_password="x"
+address=
+address
+"bc1qs2puhh70c49u9gfluxwu9rdf43jv3n7eqf2awa"
+pool_url=
+pool_url
+
+pool
+"stratum+tcp://us-east.stratum.slushpool.com:3333"
+worker_name=
+worker_name
+
+worker
+"worker1"
+worker_password=
+worker_password
+
+worker
+"x"
 
 # Install dependencies
 sudo apt-get update
 sudo apt-get install -y build-essential libusb-1.0-0-dev
 
+
+sudo apt-get update
+sudo apt-get install -y build-essential libusb-1.0-0
+
+sudo apt-get update
+sudo apt-get install -y build-essential libusb-1.0
+
+sudo apt-get update
+sudo apt-get install -y build-essential libusb-
+
+sudo apt-get update
+sudo apt-get install -y build-essential libusb
+
+sudo apt-get update
+sudo apt-get install -y build-essential lib
+
+sudo apt-get update
+sudo apt-get install -y build-essential
+
+sudo apt-get update
+sudo apt-get install -y build-
+
+sudo apt-get update
+sudo apt-get install -y build
+
+sudo apt-get update
+sudo apt-get install -y
+
+sudo apt-get update
+sudo apt-get install -
+
+sudo apt-get update
+sudo apt-get
+
+sudo apt-get update
+sudo apt
+
+sudo apt-get update
+sudo
+
+sudo apt-get update
+
+sudo apt-get
+
+sudo apt
+
+sudo
 # Download and build cgminer
 wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
 tar -zxvf v4.10.0.tar.gz
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v4.10.0.tar
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v4.10.0
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v4.10.
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v4.10
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v4.
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v4
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf v
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zxvf
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -zx
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar -
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+tar
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar.gz
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0.tar
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.0
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10.
+
+wget https://github.com/ckolivas/cgminer/archive/v4.10
+
+wget https://github.com/ckolivas/cgminer/archive/v4.
+
+wget https://github.com/ckolivas/cgminer/archive/v4
+
+wget https://github.com/ckolivas/cgminer/archive/v
+
+wget https://github.com/ckolivas/cgminer/archive
+
+wget https://github.com/ckolivas/cgminer
+
+wget https://github.com/ckolivas/cg
+
+wget https://github.com/ckolivas/c
+
+wget https://github.com/ckolivas
+
+wget https://github.com/ckol
+
+wget https://github.com/ck
+
+wget https://github.com/
+
+wget https://github.com
+
+wget https://github
+
+wget https://
+
+wget https
+
+wget
 cd cgminer-4.10.0
 ./autogen.sh
 ./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury --enable-hashfast
 make
 
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury --enable-hashfast
+make
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury --enable-hashfast
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury --enable-hash
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury --
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitfury
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bitf
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --enable-bit
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon --
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-avalon
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --enable-aval
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex --
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-ztex
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --enable-z
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-modminer
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --enable-mod
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus --
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-icarus
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --enable-ic
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce --
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bitforce
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --enable-bit
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --
+./autogen.sh
+./configure --enable-opencl --enable-scrypt --
+
+./autogen.sh
+./configure --enable-opencl --enable-scrypt
+
+./autogen.sh
+./configure --enable-opencl --enable-sc
+
+./autogen.sh
+./configure --enable-opencl --
+./autogen.sh
+./configure --enable-opencl
+
+./autogen.sh
+./configure --enable-open
+
+./autogen.sh
+./configure --
+./autogen.sh
+./configure --
+
+./autogen.sh
+./configure
+
+./autogen.sh
+
+./autogen
+
+./
 # Start mining
-./cgminer -o $pool_url -u $worker_name -p $worker_password --bmsc-options 115200:0.59 -S all --api-listen
+./cgminer -o 
+./cgminer -o $
+
+./cgminer -o
+
+./cgminer -
+
+./cgminer
+
+./cg
+
+./
+$pool_url -u $worker_name -p $worker_password --bmsc-options 115200:0.59 -S all --api-listen
 
 # Send mined Bitcoin to the specified address
-bitcoin-cli sendtoaddress $address $(bitcoin-cli getbalance)
+bitcoin-cli sendtoaddress 
+bitcoin-cli sendtoaddress $
+
+bitcoin-cli sendtoaddress
+
+bitcoin-cli sendto
+
+bitcoin-cli send
+
+bitcoin-cli
+
+bitcoin
+$address $(bitcoin-cli getbalance)
